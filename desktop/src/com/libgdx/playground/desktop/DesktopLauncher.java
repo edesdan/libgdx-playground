dfe3e832fc64b819a40c11a45261b4f06cfdc73b
edesdan <danieledesanctis@gmail.com>
2015-05-25 17:24:18 +0100

    First commit to the repository, all files produced by gdx-setup.jar using libgdx 1.6.1

---
+++ desktop/src/com/libgdx/playground/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.libgdx.playground.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.libgdx.playground.Playground;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Playground(), config);
+	}
+}
